8b5a3a388ab82cfae1a66f35a82891a95927d034
Akila Ramachandran <akila@Akilas-MacBook-Air.local>
2023-12-31 13:04:08 -0800

    Playing this game with one player is fully functional and scores are saving properly and if you roll a 1 game will break

--- main.py
+++ main.py
@@ -9,3 +9,16 @@ print(players)
 
 print(f"Player {turnofplayer}'s turn has just started")
 print(f"Your total score is {score}")
+
+while True:
+        wouldyouliketoroll = input("Would you like to roll (y/n)")
+        if wouldyouliketoroll == "y":
+            randomnumber = random.randint(1,6)
+            score = players[turnofplayer-1].append(randomnumber)
+            print(f"You rolled a: {randomnumber}")
+            sumofscores = sum(players[turnofplayer-1])
+            print(f"Your score is: {sumofscores}")
+            if randomnumber == 1:
+                  break
+        else:
+              break
